7216d5bd76dc7e73b01b7d8b3f377f99c17ea033
Matthias Winkler <mwinkler@gmx.ch>
2022-10-12 21:30:42 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,17 @@
 let power = 0
+let color = 0
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     power = Math.min(power + 10, 100)
-    motors.dualMotorPower(Motor.AB, power)
+    updateMotor()
 })
+function updateMotor () {
+    motors.dualMotorPower(Motor.AB, power)
+    color = 255 * (power / 100)
+    basic.setLedColor(basic.rgb(color, color, color))
+}
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     power = Math.max(power - 10, 0)
-    motors.dualMotorPower(Motor.AB, power)
+    updateMotor()
 })
 basic.forever(function () {
 	
